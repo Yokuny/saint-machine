da9ae7cc701547d229931ef6dafce6ebbe57d21d
yokuny <felipe.vni@hotmail.com>
2021-02-10 20:33:26 -0300

    a correction in marging of all page

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import ContentField from './pages/_ContentField'
 import _WorksSlide from './pages/_WorksSlide'
 export default function FirstPage() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3">
-        <div className="col-span-3">
-            <_WorksSlide title="Posts" />
-        </div>
-        <div className="lg:mt-14 mt-8 col-span-3 md:col-span-2">
-            <ContentField />
-        </div>
+    <div className="flex justify-content items-center">
+      <div className="grid grid-cols-1 lg:grid-cols-3">
+          <div className="col-span-3">
+              <_WorksSlide title="Posts" />
+          </div>
+          <div className="lg:mt-14 mt-8 col-span-3 md:col-span-2">
+              <ContentField />
+          </div>
+      </div>
     </div>
   );
 };
