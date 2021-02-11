acaefaf37516addd77b1ba90fb8fff89748f56b3
yokuny <felipe.vni@hotmail.com>
2021-02-10 21:38:53 -0300

    gooo

--- pages/pages/_ContentField.js
+++ pages/pages/_ContentField.js
@@ -4,7 +4,7 @@ export default function ContentField(){
     return(
         <div className="flex flex-col justify-start bg-gray-800 rounded">
             <ContentFieldTitle title="Web log" />
-            <ul className="mx-6 mb-8">
+            <ul className="mx-6 mb-8 mt-2">
                 <MyPostsLinkContent />
             </ul>
         </div>
